16c7a0c64cca4feb1281f0a352511ec81e417747
arkondoma <arkondoma@gmail.com>
2018-03-17 21:27:06 -0400

    Added UnitConversionTest, tests UnitConversionActivity, has first passing UI test

---
+++ app/src/androidTest/java/com/simplemobiletools/calculator/UnitConversionTest.java
@@ -0,0 +1,54 @@
+package com.simplemobiletools.calculator;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.simplemobiletools.calculator.activities.MainActivity;
+import com.simplemobiletools.calculator.activities.UnitConversionActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.Espresso.onData;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+
+@RunWith(AndroidJUnit4.class)
+public class UnitConversionTest {
+    @Rule public final ActivityTestRule<UnitConversionActivity> activity = new ActivityTestRule<>(UnitConversionActivity.class);
+
+    @Test
+    public void testDistanceConvert() {
+        press(R.id.conversion_type_spinner);
+        onData(allOf(is(instanceOf(String.class)), is("Distance"))).perform(click());
+        press(R.id.units_before_spinner);
+        onData(allOf(is(instanceOf(String.class)), is("Kilometers"))).perform(click());
+        press(R.id.btn_1);
+        press(R.id.btn_equals);
+        checkResult("1000.0");
+    }
+
+    private void press(int id) {
+        onView(withId(id)).perform(click());
+    }
+
+    private void longPress(int id) {
+        onView(withId(id)).perform(longClick());
+    }
+
+    private void checkResult(String desired) {
+        onView(withId(R.id.after)).check(matches(withText(desired)));
+    }
+
+    private void checkFormula(String desired) {
+        onView(withId(R.id.formula)).check(matches(withText(desired)));
+    }
+}
