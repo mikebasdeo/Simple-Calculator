9d5bba80377fa5c529830b1bdba66164c361b36b
ModestosV <modestosvergados@gmail.com>
2018-03-17 23:02:22 -0400

    Completely removed dependency on old implementation of lengthConversion.java

--- app/src/main/kotlin/com/simplemobiletools/calculator/activities/UnitConversionActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/calculator/activities/UnitConversionActivity.kt
@@ -10,7 +10,10 @@ import android.widget.ArrayAdapter
 import android.widget.Spinner
 import com.simplemobiletools.calculator.R
 import com.simplemobiletools.calculator.conversions.*
-import com.simplemobiletools.calculator.helpers.*
+import com.simplemobiletools.calculator.helpers.CONSTANT
+import com.simplemobiletools.calculator.helpers.Calculator
+import com.simplemobiletools.calculator.helpers.CalculatorImpl
+import com.simplemobiletools.calculator.helpers.Formatter
 import com.simplemobiletools.commons.extensions.performHapticFeedback
 import kotlinx.android.synthetic.main.activity_unit_conversion.*
 
@@ -32,9 +35,6 @@ class UnitConversionActivity : SimpleActivity(), Calculator {
         setContentView(R.layout.activity_unit_conversion)
         calc = CalculatorImpl(this, applicationContext)
 
-        //create lengthConversion helper object.
-        var lengthConversion : lengthConversion = lengthConversion()
-
         //hookup for keypad
         getButtonIds().forEach {
             it.setOnClickListener { calc.numpadClicked(it.id); checkHaptic(it) }
@@ -53,16 +53,16 @@ class UnitConversionActivity : SimpleActivity(), Calculator {
         }
 
 
-        //Three drop down menus. The conversionChoiceSpiner changes the other two automatically.
-        val conversionChoiceSpinner: Spinner
-        val unitsBeforeSpinner: Spinner
-        val unitsAfterSpinner: Spinner
+        //Three drop down menus. The conversionChoiceSpinner changes the other two automatically.
+        val conversionChoiceSpinner: Spinner = findViewById(R.id.conversion_type_spinner)
+        val unitsBeforeSpinner: Spinner = findViewById(R.id.units_before_spinner)
+        val unitsAfterSpinner: Spinner = findViewById(R.id.units_after_spinner)
 
         //Main List for conversion choices from helper.
-        val conversionChoiceList = lengthConversion.conversionChoiceList
+        val conversionChoiceList = listOf("Distance", "Speed", "Time", "Volume", "Weight")
 
         //Empty list that will be populated with the relevant conversion units.
-        var unitList = ArrayList<String>()
+        val unitList = ArrayList<String>()
 
         val choiceAdapter: ArrayAdapter<String>
         val beforeAdapter: ArrayAdapter<String>
@@ -74,18 +74,12 @@ class UnitConversionActivity : SimpleActivity(), Calculator {
         beforeAdapter = ArrayAdapter(this, R.layout.spinner_item, unitList)
         afterAdapter = ArrayAdapter(this, R.layout.spinner_item, unitList)
 
-        //Connect to layout.
-        conversionChoiceSpinner = findViewById<Spinner>(R.id.conversion_type_spinner)
-        unitsBeforeSpinner = findViewById<Spinner>(R.id.units_before_spinner)
-        unitsAfterSpinner = findViewById<Spinner>(R.id.units_after_spinner)
-
         //Connect each spinner to its respective adapter.
         conversionChoiceSpinner.adapter = choiceAdapter
         unitsBeforeSpinner.adapter = beforeAdapter
         unitsAfterSpinner.adapter = afterAdapter
 
         conversionChoiceSpinner.onItemSelectedListener = object : OnItemSelectedListener {
-
             override fun onItemSelected(arg0: AdapterView<*>, arg1: View, arg2: Int, arg3: Long) {
                 val s = conversionChoiceSpinner.getItemAtPosition(arg2).toString()
                 when (s){
@@ -109,6 +103,12 @@ class UnitConversionActivity : SimpleActivity(), Calculator {
                         unitsBeforeSpinner.setSelection(0)}
                     "Time" -> {
                         converter = TimeConversion()
+                        unitList.clear()
+                        for(m in converter.getMap())
+                            unitList.add(m.key)
+                        unitsBeforeSpinner.setSelection(0)}
+                    "Volume" -> {
+                        converter = VolumeConversion()
                         unitList.clear()
                         for(m in converter.getMap())
                             unitList.add(m.key)
