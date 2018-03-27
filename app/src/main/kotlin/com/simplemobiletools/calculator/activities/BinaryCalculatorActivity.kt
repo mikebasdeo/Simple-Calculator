1a8544716ffb8ba3c173012ea81c02ec15ece8f4
mikebasdeo <mikebasdeo@gmail.com>
2018-03-27 17:24:34 -0400

    Removed Calculator object dependency. Cursor now working properly.

--- app/src/main/kotlin/com/simplemobiletools/calculator/activities/BinaryCalculatorActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/calculator/activities/BinaryCalculatorActivity.kt
@@ -1,31 +1,21 @@
 package com.simplemobiletools.calculator.activities
-
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Paint
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import com.simplemobiletools.calculator.R
-import com.simplemobiletools.calculator.helpers.Calculator
-import com.simplemobiletools.calculator.helpers.CalculatorImpl
-import com.simplemobiletools.calculator.helpers.Formatter
-import com.simplemobiletools.commons.extensions.performHapticFeedback
 import kotlinx.android.synthetic.main.activity_binary_calculator.*
-import android.text.InputType
-import android.view.View.OnTouchListener
 import android.widget.Toast
 import com.simplemobiletools.calculator.helpers.BinaryCalculator
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 
 
-class BinaryCalculatorActivity : SimpleActivity(), Calculator {
-
-
-    private lateinit var calc: CalculatorImpl
+class BinaryCalculatorActivity : SimpleActivity() {
 
     private lateinit var binaryCalculator: BinaryCalculator
     private var vibrateOnButtonPress = true
-    private lateinit var lastTouched: TextView
+    private lateinit var lastTouched: EditText
     private fun getButtonIds() = arrayOf(btn_0, btn_1)
 
     @SuppressLint("MissingSuperCall")
@@ -33,41 +23,34 @@ class BinaryCalculatorActivity : SimpleActivity(), Calculator {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_binary_calculator)
-        calc = CalculatorImpl(this, applicationContext)
         binaryCalculator = BinaryCalculator()
-
-
-        binary_number_1.setPaintFlags(binary_number_1.getPaintFlags() or Paint.UNDERLINE_TEXT_FLAG)
-        binary_number_2.setPaintFlags(binary_number_2.getPaintFlags() or Paint.UNDERLINE_TEXT_FLAG)
-
         lastTouched = binary_number_1
 
-        //hookup for keypad
-        getButtonIds().forEach {
-            it.setOnClickListener { calc.numpadClicked(it.id); checkHaptic(it) }
+        binary_number_1.paintFlags = binary_number_1.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+        binary_number_2.paintFlags = binary_number_2.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+
+        btn_0.setOnClickListener { lastTouched.text.insert(0, "0")}
+        btn_1.setOnClickListener { lastTouched.text.insert(0, "1") }
+
+        binary_number_1.setOnTouchListener { input, event ->
+            input.onTouchEvent(event)
+            lastTouched = binary_number_1
+            val noKeyboard = input.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            noKeyboard.hideSoftInputFromWindow(input.windowToken, 0)
         }
 
-        binary_number_1.setOnTouchListener({ v, event ->
-            lastTouched = binary_number_1
-            val inType = binary_number_1.getInputType()
-            binary_number_1.setInputType(InputType.TYPE_NULL)
-            binary_number_1.onTouchEvent(event)
-            binary_number_1.setInputType(inType)
-            true // consume touch event
-        })
-
-        binary_number_2.setOnTouchListener({ v, event ->
+        binary_number_2.setOnTouchListener { input, event ->
+            input.onTouchEvent(event)
             lastTouched = binary_number_2
-            val inType = binary_number_2.getInputType()
-            binary_number_2.setInputType(InputType.TYPE_NULL)
-            binary_number_2.onTouchEvent(event)
-            binary_number_2.setInputType(inType)
-            true
-        })
+            val noKeyboard = input.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            noKeyboard.hideSoftInputFromWindow(input.windowToken, 0)
+        }
 
         btn_del.setOnClickListener {
-            lastTouched.text = lastTouched.text.dropLast(1)
+            val text = lastTouched.getText().toString()
+            lastTouched.setText(text.substring(0, text.length - 1))
         }
+
         btn_all_clear.setOnClickListener {
             binary_result.text = ""
             binary_number_2.setText("")
@@ -82,41 +65,33 @@ class BinaryCalculatorActivity : SimpleActivity(), Calculator {
                 "Please enter a valid binary number".toast(this)
                 lastTouched = binary_number_1
                 binary_number_1.requestFocus()
-
             }else if (binary_number_2.text.isNullOrBlank()){
                 "Please enter a valid binary number".toast(this)
                 lastTouched = binary_number_2
                 binary_number_2.requestFocus()
-
             }else{
                 binary_result.text = binaryCalculator.addBinary(binary_number_1.text.toString(), binary_number_2.text.toString())
             }
         }
-    }
 
-    override fun setValue(value: String, context: Context) {
-        lastTouched.text = value
-    }
+        btn_minus.setOnClickListener{
 
-    // used only by Robolectric
-    override fun setValueDouble(d: Double) {
-        calc.setValue(Formatter.doubleToString(d))
-    }
-
-    override fun setFormula(value: String, context: Context) {
-        if(value == ""){
-            lastTouched.text = ""
+            if(binary_number_1.text.isNullOrBlank()){
+                "Please enter a valid binary number".toast(this)
+                lastTouched = binary_number_1
+                binary_number_1.requestFocus()
+            }else if (binary_number_2.text.isNullOrBlank()){
+                "Please enter a valid binary number".toast(this)
+                lastTouched = binary_number_2
+                binary_number_2.requestFocus()
+            }else{
+                binary_result.text = binaryCalculator.subtractBinary(binary_number_1.text.toString(), binary_number_2.text.toString())
+            }
         }
-        else{
-           lastTouched.text = lastTouched.text.toString() + value
-        }
+
+
     }
 
-    private fun checkHaptic(view: View) {
-        if (vibrateOnButtonPress) {
-            view.performHapticFeedback()
-        }
-    }
     fun Any.toast(context: Context) {
         Toast.makeText(context, this.toString(), Toast.LENGTH_LONG).show()
     }
