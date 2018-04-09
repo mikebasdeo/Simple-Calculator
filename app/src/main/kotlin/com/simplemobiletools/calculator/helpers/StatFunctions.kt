3c9399fecef9c52f173f396c5c299725878b0fd1
ModestosV <modestosvergados@gmail.com>
2018-04-08 22:22:28 -0400

    #85 Realized I was sorting Strings and not Doubles. Now, the methods still take in a list of Doubles represented as strings, but they can now be converted to Doubles and then sorted with sortListOfStringsOfDoubles #93. Testing needed for Mean, Median, Mode, Range. @lambhunter0

--- app/src/main/kotlin/com/simplemobiletools/calculator/helpers/StatFunctions.kt
+++ app/src/main/kotlin/com/simplemobiletools/calculator/helpers/StatFunctions.kt
@@ -10,7 +10,6 @@ fun updateStats(results: ArrayList<String>){
 fun getMean(results: ArrayList<String>): String{
     if(results.isEmpty())
         return ""
-
     var avg = 0.0
     for(r in results){
         avg += r.toDouble()
@@ -21,12 +20,11 @@ fun getMean(results: ArrayList<String>): String{
 fun getMedian(results: ArrayList<String>): String{
     if(results.isEmpty())
         return ""
-    
-    results.sort()
-    return if(results.size % 2 == 0)
-        ((results[results.size/2].toDouble() + results[results.size/2 - 1].toDouble())/2).toString()
+    val listOfSortedResults = sortListOfStringsOfDoubles(results)
+    return if(listOfSortedResults.size % 2 == 0)
+        ((listOfSortedResults[listOfSortedResults.size/2] + listOfSortedResults[listOfSortedResults.size/2 - 1])/2).toString()
     else
-        (results[results.size/2].toDouble()).toString()
+        (listOfSortedResults[listOfSortedResults.size/2]).toString()
 }
 
 fun getMode(results: ArrayList<String>): String{
@@ -34,7 +32,7 @@ fun getMode(results: ArrayList<String>): String{
         return ""
 
     val storeValues = hashMapOf<String, Int>()
-    val listOfModes = mutableListOf<String>()
+    val listOfModes = arrayListOf<String>()
     var highestCount = 0
 
     //Get count of each occurrence
@@ -46,7 +44,6 @@ fun getMode(results: ArrayList<String>): String{
             storeValues[r] = count!! + 1
         }
     }
-
     //Store highest count
     for(s in storeValues){
         if(highestCount < s.value)
@@ -57,14 +54,22 @@ fun getMode(results: ArrayList<String>): String{
         if(s.value == highestCount)
             listOfModes.add(s.key)
     }
-    listOfModes.sort()
-    return listOfModes.toString()
+    val listOfSortedModes = sortListOfStringsOfDoubles(listOfModes)
+    return listOfSortedModes.toString()
 }
 
 fun getRange(results: ArrayList<String>): String {
     if(results.isEmpty())
         return ""
+    val listOfSortedModes = sortListOfStringsOfDoubles(results)
+    return (listOfSortedModes.last() - listOfSortedModes.first()).toString()
+}
 
-    results.sort()
-    return (results.last().toDouble() - results.first().toDouble()).toString()
+private fun sortListOfStringsOfDoubles(listOfStringOfDoubles: ArrayList<String>): ArrayList<Double> {
+    val listOfDoubles = ArrayList<Double>()
+    for(l in listOfStringOfDoubles){
+        listOfDoubles.add(l.toDouble())
+    }
+    listOfDoubles.sort()
+    return listOfDoubles
 }
