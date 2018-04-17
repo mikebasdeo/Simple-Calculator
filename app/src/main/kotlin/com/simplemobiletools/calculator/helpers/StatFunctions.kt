6523aaff1119f18894f15428c7136376d9efbcf9
ModestosV <modestosvergados@gmail.com>
2018-04-16 21:46:36 -0400

    #84 Fix some merge errors that caused crashes. Added some TODOs for stats that are broken

--- app/src/main/kotlin/com/simplemobiletools/calculator/helpers/StatFunctions.kt
+++ app/src/main/kotlin/com/simplemobiletools/calculator/helpers/StatFunctions.kt
@@ -7,63 +7,68 @@ fun updateStats(results: ArrayList<String>){
     getRange(results)
 }
 
+//TODO: Fix all these methods
+
 fun getMean(results: ArrayList<String>): String{
     if(results.isEmpty())
         return ""
     var avg = 0.0
     for(r in results) {
-        avg += r.toDouble()
+        avg += r.replace(",","").toDouble()
     }
 
     return String.format(java.util.Locale.US,"%.2f", avg)
 }
 
 fun getMedian(results: ArrayList<String>): String{
-    if(results.isEmpty())
-        return ""
-    val listOfSortedResults = sortListOfStringsOfDoubles(results)
-    return if(listOfSortedResults.size % 2 == 0)
-        ((listOfSortedResults[listOfSortedResults.size/2] + listOfSortedResults[listOfSortedResults.size/2 - 1])/2).toString()
-    else
-        (listOfSortedResults[listOfSortedResults.size/2]).toString()
+//    if(results.isEmpty())
+//        return ""
+//    val listOfSortedResults = sortListOfStringsOfDoubles(results)
+//    return if(listOfSortedResults.size % 2 == 0)
+//        ((listOfSortedResults[listOfSortedResults.size/2] + listOfSortedResults[listOfSortedResults.size/2 - 1])/2).toString()
+//    else
+//        (listOfSortedResults[listOfSortedResults.size/2]).toString()
+    return "fix"
 }
 
 fun getMode(results: ArrayList<String>): String{
-    if(results.isEmpty())
-        return ""
-
-    val storeValues = hashMapOf<String, Int>()
-    val listOfModes = arrayListOf<String>()
-    var highestCount = 0
-
-    //Get count of each occurrence
-    for(r in results){
-        if(storeValues[r] == null)
-            storeValues[r] = 1
-        else{
-            val count = storeValues[r]
-            storeValues[r] = count!! + 1
-        }
-    }
-    //Store highest count
-    for(s in storeValues){
-        if(highestCount < s.value)
-            highestCount = s.value
-    }
-    //Every number with an equal highest count is added to return list
-    for(s in storeValues){
-        if(s.value == highestCount)
-            listOfModes.add(s.key)
-    }
-    val listOfSortedModes = sortListOfStringsOfDoubles(listOfModes)
-    return listOfSortedModes.toString()
+//    if(results.isEmpty())
+//        return ""
+//
+//    val storeValues = hashMapOf<String, Int>()
+//    val listOfModes = arrayListOf<String>()
+//    var highestCount = 0
+//
+//    //Get count of each occurrence
+//    for(r in results){
+//        if(storeValues[r] == null)
+//        storeValues[r] = 1
+//    else{
+//        val count = storeValues[r]
+//        storeValues[r] = count!! + 1
+//    }
+//}
+////Store highest count
+//for(s in storeValues){
+//    if(highestCount < s.value)
+//        highestCount = s.value
+//}
+////Every number with an equal highest count is added to return list
+//for(s in storeValues){
+//    if(s.value == highestCount)
+//        listOfModes.add(s.key)
+//}
+//val listOfSortedModes = sortListOfStringsOfDoubles(listOfModes)
+//return listOfSortedModes.toString()
+    return "fix"
 }
 
 fun getRange(results: ArrayList<String>): String {
-    if(results.isEmpty())
-        return ""
-    val listOfSortedModes = sortListOfStringsOfDoubles(results)
-    return (listOfSortedModes.last() - listOfSortedModes.first()).toString()
+//    if(results.isEmpty())
+//        return ""
+//    val listOfSortedModes = sortListOfStringsOfDoubles(results)
+//    return (listOfSortedModes.last() - listOfSortedModes.first()).toString()
+    return "fix"
 }
 
 private fun sortListOfStringsOfDoubles(listOfStringOfDoubles: ArrayList<String>): ArrayList<Double> {
